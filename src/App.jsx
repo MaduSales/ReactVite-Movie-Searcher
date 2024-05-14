299abdc6a866efba585c3916024d38c03be0d088
Maria Eduarda <maria.mirandadev@gmail.com>
2024-05-14 19:29:20 -0300

    configuracoes para deploy

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import BuscarFilmes from './components/BuscaFilmes/BuscaFilmes'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className='App'>
+      <BuscarFilmes/>
+    </div>
+  )
+}
